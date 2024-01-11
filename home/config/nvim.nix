2a636dec0d65377485874f85c5575218b7d4189d
= <=>
2024-01-11 10:24:42 +0000

    add neovim config

--- home/config/nvim.nix
+++ home/config/nvim.nix
@@ -3,6 +3,13 @@
 	programs.neovim = {
 		enable = true;
 		defaultEditor = true;
-		extraLuaConfig = lib.fileContents ./nvim/init.lua;
+		extraLuaConfig = ''
+			${lib.fileContents ./nvim/init.lua}
+			${lib.fileContents ./nvim/lua/bubbles.lua}
+			${lib.fileContents ./nvim/lua/plugin.lua}
+			${lib.fileContents ./nvim/lua/setup_global.lua}
+			${lib.fileContents ./nvim/lua/setup_lsp.lua}
+			${lib.fileContents ./nvim/lua/setup_telescope.lua}
+		''
 	};
 }
